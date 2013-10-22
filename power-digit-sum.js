e92c88587168b1dd3066df628112d42e8bf58f2b
Jackson Gariety <personal@jacksongariety.com>
2013-10-22 16:43:07 -0700

    Changes to power-digit-sum.

--- power-digit-sum.js
+++ power-digit-sum.js
@@ -2,11 +2,11 @@
  * Problem 16: Power d s
  */
 
-!function (n) {
+!function (e) {
   var n = [1]
     , s = 0
   
-  var i = 1000
+  var i = e || 1000
   while (i--) {
     var o = 0
     
@@ -32,5 +32,5 @@
     s += n[k]
   }
   
-  return console.log(s)
+  return s
 }()
